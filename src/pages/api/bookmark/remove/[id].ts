bf7f9177ac4ae3bb28bc00f6eb14a6a25a191a4a
内藤祐馬 <naitoyuma@outlook.jp>
2023-03-20 08:45:34 +0900

    [fix]中間テーブルを直接deleteManyで削除する方法を変更。article.update()からリレーション経由で削除する方法にする

--- src/pages/api/bookmark/remove/[id].ts
+++ src/pages/api/bookmark/remove/[id].ts
@@ -6,24 +6,29 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
   const session = await getSession({ req });
 
   if (session?.user?.email) {
+    // // email一致条件でdisconenctができなかった
+    // // ログインユーザーIDを別途取得してからid指定で連携を解除する
+    // // userIdとarticleIdでdelete()が実行できなかったのはuniqueが保障されていなため
+    // // deleteManyで複数一致を許容すれば実行可能
     const user = await prisma.user.findUnique({
       where: {
         email: session?.user?.email,
       },
     });
-    // email一致条件でdisconenctができなかった
-    // ログインユーザーIDを別途取得してからid指定で連携を解除する
-    // userIdとarticleIdでdelete()が実行できなかったのはuniqueが保障されていなため
-    // deleteManyで複数一致を許容すれば実行可能
     const userId = user!.id;
-    const result = await prisma.bookmark.deleteMany({
+    const result = await prisma.article.update({
       where: {
-        userId: userId,
-        articleId: Number(req.query.id),
+        id: Number(req.query.id),
+      },
+      data: {
+        isLikedUsers: {
+          deleteMany: {
+            userId: userId,
+          },
+        },
       },
     });
 
-    console.log(userId);
     res.json(result);
   } else {
     res.status(401).send({ message: 'Unauthorized' });
