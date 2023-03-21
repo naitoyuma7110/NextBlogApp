2658bfe8498b2e344140f4f476c8c13b19d2dcf1
Naito Yuma <naitoyuma1118@gmail.com>
2023-03-22 01:34:01 +0900

    [add]seed,記事一覧に投稿者情報の追加、表示

--- prisma/data/articles.ts
+++ prisma/data/articles.ts
@@ -17,24 +17,4 @@ export const articles = [
       'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur',
     authorUserId: 1,
   },
-  {
-    title: 'Using Next.js with TypeScript',
-    content:
-      'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there is not anything embarrassing hidden in the middle of text.',
-  },
-  {
-    title: 'Getting started with Lambda',
-    content:
-      'Duis enim nisi, tincidunt id purus sed, tempus sagittis tellus. Praesent porta quis nibh eget hendrerit. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed blandit scelerisque malesuada. Quisque at ipsum suscipit, hendrerit massa ut, sagittis magna',
-  },
-  {
-    title: 'Deploying with GitHub Actions',
-    content:
-      'Ut porta, tortor a egestas ullamcorper, nulla augue blandit nulla, in luctus quam nibh vel eros. Donec id nisi vel enim efficitur egestas a eu turpis. Nam libero magna, porttitor sed imperdiet placerat, scelerisque eget elit. Fusce efficitur metus ut ultricies mattis. Pellentesque consectetur dapibus suscipit. Sed pulvinar lacus vel semper mattis.',
-  },
-  {
-    title: 'Supabase From Scratch',
-    content:
-      'Donec ut orci sed est viverra interdum. Phasellus et erat diam. Nam ornare faucibus orci eu cursus. Fusce lacinia, dui vitae molestie fringilla, quam sapien blandit enim, eget finibus arcu ligula sit amet enim. Vivamus non congue arcu. Curabitur nisl sem, vestibulum in diam vel, tristique posuere sem',
-  },
 ];
--- src/types/ArticlesProps.ts
+++ src/types/ArticlesProps.ts
@@ -2,4 +2,5 @@ import { Article, User } from '@prisma/client';
 
 export type ArticlesProps = Article & {
   isLikedUsers: User[];
+  author: User;
 };
